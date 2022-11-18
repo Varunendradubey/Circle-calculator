4f21a9e81967064d9b857ca981dde1e63d21edeb
Varunendradubey <118537432+Varunendradubey@users.noreply.github.com>
2022-11-18 18:57:18 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
-import javax.xml.stream.events.EndDocument;
-
 public class Project{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 System.out.print("Use this to calculate the radius, circumference, area, or diameter of a circle.  (type 'C' to continue)");
 String input=sc.nextLine();
-if(input=="C"){
+if(input.compareTo("C")==0){
     System.out.print("What would you like to calculate? (Enter 'radius', 'circumference', 'area', diameter')");
 }
 else{
     System.out.println("input is invalid");
     System.out.print("What would you like to calculate? (Enter 'radius', 'circumference', 'area', diameter')");
 }
+
     }
 }  
